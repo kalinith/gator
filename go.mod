329aca6e8d496b3cbf7118ed659af9cb5a0ff5b5
kalinith <kalinith@gmail.com>
2025-03-23 21:23:14 +0200

    wip
    working on the cli commands

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kalinith/Gator
+module Gator
 
 go 1.23.0
--- main.go
+++ main.go
@@ -1,10 +1,69 @@
 package main
 import (
-	"github.com/kalinith/Gator/internal/config"
+	"Gator/internal/config"
+	"os"
 	"fmt"
 )
 
+type state struct {
+	config *Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handler map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	if name == "" {
+		fmt.Println("Error: Empty command name provided")
+		return
+	}
+	if f == nil {
+		fmt.Println("function cannot be empty")
+		return
+	}
+	c[name] = f
+	return
+} // This method registers a new handler function for a command name.
+
+func (c *commands) run(s *state, cmd command) error {
+	f, err := c[cmd.name]
+	if err != nil {
+		return fmt.Errorf("unable to retrieve command: %s", err)
+	}
+	err := f(s, command)
+	if err != nil {
+		return fmt.Errorf("Failed to execute command: %s", err)
+	}
+	return nil
+} // This method runs a given command with the provided state if it exists.
+
+
+func handlerLogin(s *state, cmd command) error {
+	if len(os.Args) < 3 {
+		return fmt.Errorf("no username provided for login")
+	}
+	login := os.Args[1]
+	err := s.config.SetUser(login)
+	if err != nil {
+		return fmt.Errorf("unable to set Current User: %s", err)
+	}
+	fmt.Printf("user has been set to %s\n", login)
+	return nil
+}
+
 func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("no arguments provided")
+		os.Exit(1)
+	}
+
 	conf, err := config.Read()
 	if err != nil {
 		fmt.Printf("Error reading config:%s", err)
