5a1e54213123f70f7f9a831a3e9457ce81d43705
kalinith <kalinith@gmail.com>
2025-03-26 17:01:04 +0200

    compile test
    partial completion of cli framework

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 type state struct {
-	config *Config
+	config *config.Config
 }
 
 type command struct {
@@ -27,16 +27,16 @@ func (c *commands) register(name string, f func(*state, command) error) {
 		fmt.Println("function cannot be empty")
 		return
 	}
-	c[name] = f
+	c.handler[name] = f
 	return
 } // This method registers a new handler function for a command name.
 
 func (c *commands) run(s *state, cmd command) error {
-	f, err := c[cmd.name]
-	if err != nil {
-		return fmt.Errorf("unable to retrieve command: %s", err)
+	f := c.handler[cmd.name]
+	if  f == nil {
+		return fmt.Errorf("unable to retrieve command: %s", cmd.name)
 	}
-	err := f(s, command)
+	err := f(s, cmd)
 	if err != nil {
 		return fmt.Errorf("Failed to execute command: %s", err)
 	}
@@ -58,27 +58,31 @@ func handlerLogin(s *state, cmd command) error {
 }
 
 func main() {
+	var args []string
+	conf := &config.Config{}
+	var err Error
 
 	if len(os.Args) < 2 {
 		fmt.Println("no arguments provided")
 		os.Exit(1)
 	}
+	if len(os.Args) > 2 {
+		args = os.Args[2:]
+	} else {
+		args = []string{}
+	}
+
+	cmd := &command{os.Args[1], args,}
 
 	conf, err := config.Read()
 	if err != nil {
 		fmt.Printf("Error reading config:%s", err)
 		return
 	}
+	systemstate := &state{conf,}
 
-	err = conf.SetUser("Kalinith")
-	if err != nil {
-		fmt.Printf("Error writing config:%s", err)
-		return
-	}
-	config2, err := config.Read()
-	if err != nil {
-		fmt.Printf("Error reading config:%s", err)
-		return
-	}
-	fmt.Println(config2.DatabaseURL, config2.CurrentUser)
+	var commandhandler map[string]func(*state, command) error
+	comm := commands{commandhandler}
+	comm.handler["login"] = handlerLogin
+
 }
